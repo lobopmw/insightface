de2b52a74634ef52b15fdb3776097e787a840362
lobopmw <starley.lobo@mail.uft.edu.br>
2025-09-11 11:31:01 -0300

    Delete files tests

--- src/test_devices.py
+++
@@ -1,5 +0,0 @@
-import torch
-print("CUDA Disponível:", torch.cuda.is_available())
-print("Versão PyTorch:", torch.__version__)
-print("Versão CUDA:", torch.version.cuda)
-print("Dispositivo disponível:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "CPU")
